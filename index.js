dcdfbf355efd0c3b3c8944dfbcb6b29c3b910621
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-10-02 21:24:02 +0300

    message

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    if (!string) return '';
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // Remove all non-alphanumeric characters except dashes, single quotes, and spaces
+    return string.replace(/[^a-zA-Z0-9\-\' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    if (!string) return '';
+    const words = string.toLowerCase().split(' ');
+    const titleized = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return titleized.join(' ');
+  }
 }
